94bba30bf5d5dbe6b199c7b50c129896f2e0df70
Dhanika Botejue <dhanikabotejue@gmail.com>
2026-03-07 09:01:00 -0500

    feat: 50 hospitals

--- lib/clearpath/mockData.ts
+++ lib/clearpath/mockData.ts
@@ -107,9 +107,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 14,
     "erBeds": 0,
     "phone": "416-962-4040",
-    "website": "https://ontario.ca",
+    "website": "https://www.caseyhouse.com",
     "specialties": [
-      "General"
+      "hiv", "palliative", "harm-reduction"
     ]
   },
   {
@@ -121,9 +121,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 500,
     "erBeds": 30,
     "phone": "416-535-8501",
-    "website": "https://ontario.ca",
+    "website": "https://www.camh.ca",
     "specialties": [
-      "General"
+      "mental-health", "addiction", "forensic-psychiatry"
     ]
   },
   {
@@ -135,9 +135,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 100,
     "erBeds": 0,
     "phone": "416-535-8501",
-    "website": "https://ontario.ca",
+    "website": "https://www.camh.ca",
     "specialties": [
-      "General"
+      "mental-health", "addiction"
     ]
   },
   {
@@ -149,9 +149,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 100,
     "erBeds": 0,
     "phone": "416-535-8501",
-    "website": "https://ontario.ca",
+    "website": "https://www.camh.ca",
     "specialties": [
-      "General"
+      "mental-health", "addiction"
     ]
   },
   {
@@ -163,9 +163,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 100,
     "erBeds": 0,
     "phone": "416-535-8501",
-    "website": "https://ontario.ca",
+    "website": "https://www.camh.ca",
     "specialties": [
-      "General"
+      "mental-health", "addiction"
     ]
   },
   {
@@ -177,9 +177,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 382,
     "erBeds": 55,
     "phone": "905-813-2200",
-    "website": "https://ontario.ca",
+    "website": "https://www.thp.ca",
     "specialties": [
-      "General"
+      "cardiac", "oncology", "obstetrics", "orthopedic", "mental-health"
     ]
   },
   {
@@ -191,9 +191,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 75,
     "erBeds": 0,
     "phone": "416-425-6220",
-    "website": "https://ontario.ca",
+    "website": "https://www.hollandbloorview.ca",
     "specialties": [
-      "General"
+      "pediatric-rehabilitation", "brain-injury", "orthopedic"
     ]
   },
   {
@@ -205,9 +205,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 370,
     "erBeds": 45,
     "phone": "416-813-1500",
-    "website": "https://ontario.ca",
+    "website": "https://www.sickkids.ca",
     "specialties": [
-      "General"
+      "pediatric", "cardiac", "oncology", "neurology", "trauma"
     ]
   },
   {
@@ -219,9 +219,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 722,
     "erBeds": 70,
     "phone": "416-242-1000",
-    "website": "https://ontario.ca",
+    "website": "https://www.hrh.ca",
     "specialties": [
-      "General"
+      "general", "cardiac", "respiratory"
     ]
   },
   {
@@ -233,9 +233,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 300,
     "erBeds": 30,
     "phone": "416-242-1000",
-    "website": "https://ontario.ca",
+    "website": "https://www.hrh.ca",
     "specialties": [
-      "General"
+      "general", "cardiac"
     ]
   },
   {
@@ -247,9 +247,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 300,
     "erBeds": 30,
     "phone": "416-242-1000",
-    "website": "https://ontario.ca",
+    "website": "https://www.hrh.ca",
     "specialties": [
-      "General"
+      "general", "cardiac"
     ]
   },
   {
@@ -261,9 +261,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 442,
     "erBeds": 40,
     "phone": "416-596-4200",
-    "website": "https://ontario.ca",
+    "website": "https://www.sinaihealth.ca",
     "specialties": [
-      "General"
+      "obstetrics", "respiratory"
     ]
   },
   {
@@ -275,9 +275,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 1325,
     "erBeds": 72,
     "phone": "416-480-6100",
-    "website": "https://ontario.ca",
+    "website": "https://sunnybrook.ca",
     "specialties": [
-      "General"
+      "trauma", "stroke", "burn", "cardiac"
     ]
   },
   {
@@ -289,9 +289,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 350,
     "erBeds": 40,
     "phone": "416-284-8131",
-    "website": "https://ontario.ca",
+    "website": "https://www.shn.ca",
     "specialties": [
-      "General"
+      "cardiac", "nephrology", "obstetrics", "mental-health", "oncology"
     ]
   },
   {
@@ -303,9 +303,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 160,
     "erBeds": 0,
     "phone": "416-226-6780",
-    "website": "https://ontario.ca",
+    "website": "https://sunnybrook.ca",
     "specialties": [
-      "General"
+      "rehabilitation", "orthopedic", "stroke"
     ]
   },
   {
@@ -317,9 +317,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 376,
     "erBeds": 50,
     "phone": "416-530-6000",
-    "website": "https://ontario.ca",
+    "website": "https://unityhealth.to",
     "specialties": [
-      "General"
+      "general", "obstetrics", "mental-health", "bariatric", "palliative"
     ]
   },
   {
@@ -331,9 +331,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 463,
     "erBeds": 55,
     "phone": "416-360-4000",
-    "website": "https://ontario.ca",
+    "website": "https://unityhealth.to",
     "specialties": [
-      "General"
+      "trauma", "neurology", "cardiac"
     ]
   },
   {
@@ -345,9 +345,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 1325,
     "erBeds": 112,
     "phone": "416-480-6100",
-    "website": "https://ontario.ca",
+    "website": "https://sunnybrook.ca",
     "specialties": [
-      "General"
+      "trauma", "stroke", "burn", "cardiac", "oncology"
     ]
   },
   {
@@ -359,9 +359,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 150,
     "erBeds": 46,
     "phone": "416-323-6400",
-    "website": "https://ontario.ca",
+    "website": "https://www.womenscollegehospital.ca",
     "specialties": [
-      "General"
+      "obstetrics", "general"
     ]
   },
   {
@@ -373,9 +373,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 400,
     "erBeds": 50,
     "phone": "416-461-8272",
-    "website": "https://ontario.ca",
+    "website": "https://www.tehn.ca",
     "specialties": [
-      "General"
+      "general", "cardiac", "bariatric", "nephrology", "respiratory"
     ]
   },
   {
@@ -387,9 +387,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 200,
     "erBeds": 43,
     "phone": "416-597-3422",
-    "website": "https://ontario.ca",
+    "website": "https://www.uhn.ca",
     "specialties": [
-      "General"
+      "rehabilitation", "stroke", "respiratory"
     ]
   },
   {
@@ -401,9 +401,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 150,
     "erBeds": 42,
     "phone": "416-597-3422",
-    "website": "https://ontario.ca",
+    "website": "https://www.uhn.ca",
     "specialties": [
-      "General"
+      "rehabilitation", "orthopedic"
     ]
   },
   {
@@ -415,9 +415,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 100,
     "erBeds": 30,
     "phone": "416-597-3422",
-    "website": "https://ontario.ca",
+    "website": "https://www.uhn.ca",
     "specialties": [
-      "General"
+      "rehabilitation", "spinal-cord"
     ]
   },
   {
@@ -429,9 +429,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 100,
     "erBeds": 32,
     "phone": "416-597-3422",
-    "website": "https://ontario.ca",
+    "website": "https://www.uhn.ca",
     "specialties": [
-      "General"
+      "rehabilitation", "cardiac"
     ]
   },
   {
@@ -443,23 +443,23 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 250,
     "erBeds": 37,
     "phone": "416-597-3422",
-    "website": "https://ontario.ca",
+    "website": "https://www.uhn.ca",
     "specialties": [
-      "General"
+      "rehabilitation", "stroke", "brain-injury"
     ]
   },
   {
     "id": "odhf-25",
-    "name": "Trillium Health Partners � Mississauga Hospital",
+    "name": "Trillium Health Partners ï¿½ Mississauga Hospital",
     "city": "mississauga",
     "latitude": 43.571585999999996,
     "longitude": -79.607685,
     "totalBeds": 278,
     "erBeds": 47,
-    "phone": "416-247-1470",
-    "website": "https://ontario.ca",
+    "phone": "905-848-7100",
+    "website": "https://www.thp.ca",
     "specialties": [
-      "General"
+      "cardiac", "neurology", "stroke", "oncology", "obstetrics"
     ]
   },
   {
@@ -471,9 +471,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 130,
     "erBeds": 41,
     "phone": "416-946-2000",
-    "website": "https://ontario.ca",
+    "website": "https://www.uhn.ca",
     "specialties": [
-      "General"
+      "oncology", "radiation-therapy", "hematology"
     ]
   },
   {
@@ -485,9 +485,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 471,
     "erBeds": 60,
     "phone": "416-340-4800",
-    "website": "https://ontario.ca",
+    "website": "https://www.uhn.ca",
     "specialties": [
-      "General"
+      "cardiac", "transplant", "respiratory"
     ]
   },
   {
@@ -499,9 +499,9 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 272,
     "erBeds": 45,
     "phone": "416-603-2581",
-    "website": "https://ontario.ca",
+    "website": "https://www.uhn.ca",
     "specialties": [
-      "General"
+      "neurology", "stroke", "orthopedic"
     ]
   },
   {
@@ -513,9 +513,163 @@ export const mockHospitals: Hospital[] = [
     "totalBeds": 300,
     "erBeds": 46,
     "phone": "416-243-3600",
+    "website": "https://www.uhn.ca",
+    "specialties": [
+      "rehabilitation", "respiratory", "amputee"
+    ]
+  },
+  {
+    "id": "odhf-30",
+    "name": "Grand River Hospital",
+    "city": "waterloo",
+    "latitude": 43.4557709,
+    "longitude": -80.51168009999999,
+    "totalBeds": 574,
+    "erBeds": 60,
+    "phone": "519-749-4300",
+    "website": "https://www.grhosp.on.ca",
+    "specialties": [
+      "general", "cardiac", "oncology", "mental-health", "renal"
+    ]
+  },
+  {
+    "id": "odhf-31",
+    "name": "St. Mary's General Hospital",
+    "city": "waterloo",
+    "latitude": 43.438288299999996,
+    "longitude": -80.5007975,
+    "totalBeds": 150,
+    "erBeds": 40,
+    "phone": "519-744-3311",
+    "website": "https://www.smgh.ca",
+    "specialties": [
+      "cardiac", "respiratory", "general"
+    ]
+  },
+  {
+    "id": "odhf-32",
+    "name": "Cambridge Memorial Hospital",
+    "city": "waterloo",
+    "latitude": 43.378516999999995,
+    "longitude": -80.32855,
+    "totalBeds": 132,
+    "erBeds": 30,
+    "phone": "519-621-2333",
+    "website": "https://www.cmh.org",
+    "specialties": [
+      "general", "obstetrics", "mental-health"
+    ]
+  },
+  {
+    "id": "odhf-33",
+    "name": "North York General Hospital - Branson Division",
+    "city": "toronto",
+    "latitude": 43.7724535,
+    "longitude": -79.44813609999999,
+    "totalBeds": 200,
+    "erBeds": 30,
+    "phone": "416-756-6000",
+    "website": "https://www.nygh.on.ca",
+    "specialties": [
+      "general", "geriatric", "rehabilitation"
+    ]
+  },
+  {
+    "id": "odhf-34",
+    "name": "North York General Hospital - General Division",
+    "city": "toronto",
+    "latitude": 43.76954979999999,
+    "longitude": -79.36320649999999,
+    "totalBeds": 420,
+    "erBeds": 48,
+    "phone": "416-756-6000",
+    "website": "https://www.nygh.on.ca",
+    "specialties": [
+      "general", "respiratory", "orthopedic"
+    ]
+  },
+  {
+    "id": "odhf-35",
+    "name": "Scarborough Health Network - General Campus",
+    "city": "toronto",
+    "latitude": 43.7557343,
+    "longitude": -79.2468315,
+    "totalBeds": 620,
+    "erBeds": 50,
+    "phone": "416-438-2911",
+    "website": "https://www.shn.ca",
+    "specialties": [
+      "general", "cardiac", "nephrology"
+    ]
+  },
+  {
+    "id": "odhf-36",
+    "name": "Scarborough Health Network - Grace Campus",
+    "city": "toronto",
+    "latitude": 43.801828799999996,
+    "longitude": -79.3087567,
+    "totalBeds": 290,
+    "erBeds": 30,
+    "phone": "416-495-2400",
+    "website": "https://www.shn.ca",
+    "specialties": [
+      "general", "rehabilitation", "geriatric"
+    ]
+  },
+  {
+    "id": "odhf-37",
+    "name": "Trillium Health Partners - Queensway Health Centre",
+    "city": "mississauga",
+    "latitude": 43.609266999999996,
+    "longitude": -79.56257,
+    "totalBeds": 300,
+    "erBeds": 40,
+    "phone": "416-259-6671",
+    "website": "https://www.thp.ca",
+    "specialties": [
+      "general", "obstetrics", "rehabilitation"
+    ]
+  },
+  {
+    "id": "odhf-38",
+    "name": "William Osler Health System - Etobicoke General",
+    "city": "toronto",
+    "latitude": 43.72929439999999,
+    "longitude": -79.5984363,
+    "totalBeds": 250,
+    "erBeds": 35,
+    "phone": "416-494-2120",
+    "website": "https://www.williamoslerhs.ca",
+    "specialties": [
+      "general", "obstetrics", "mental-health"
+    ]
+  },
+  {
+    "id": "odhf-39",
+    "name": "Don Mills Surgical Unit",
+    "city": "toronto",
+    "latitude": 43.7236162,
+    "longitude": -79.33617748,
+    "totalBeds": 50,
+    "erBeds": 0,
+    "phone": "416-441-2111",
     "website": "https://ontario.ca",
     "specialties": [
-      "General"
+      "general", "surgical"
+    ]
+  },
+  {
+    "id": "odhf-40",
+    "name": "Humber River Regional Hospital - Church St. Site",
+    "city": "toronto",
+    "latitude": 43.7086562,
+    "longitude": -79.509922,
+    "totalBeds": 200,
+    "erBeds": 25,
+    "phone": "416-242-1000",
+    "website": "https://www.hrh.ca",
+    "specialties": [
+      "general", "rehabilitation"
     ]
   }
 ];
